4baf4719c6204dda6f0357164f8c0a43a011ca9b
jeoung-youjin <150499204+jeoung-youjin@users.noreply.github.com>
2024-02-02 16:19:19 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -18,7 +18,6 @@ const allWrap = document.querySelector(".all-menu-open")
 const allBg = document.querySelector(".all-menu-bg")
 const body = document.getElementsByTagName('body')[0];
 
-
 allMenu.addEventListener("click",()=>{
   allWrap.classList.toggle("on"),
   allBg.classList.toggle("on")
@@ -26,32 +25,25 @@ allMenu.addEventListener("click",()=>{
 })
 
 //f-notice 리스트 적용 
-const fNotice = document.querySelector(".f-notice-slide > .swiper-wrapper")
-function Notice(tit,day){
-  this.tit=tit;
-  this.day=day;
-}
+let fNotice = document.querySelector(".f-notice-slide > .swiper-wrapper")
+fetch("../portfolio04/js/main.json")
+.then((response) => response.json())
+.then((json)=>{
+  data = json.하단공지;
+  let noList = '';
+  data.forEach(element => {
+    noList +=
+    makeFNoticeTemp(element);
+  });
+  fNotice.innerHTML=noList
+})
 
-const notice01 = new Notice("청춘굿즈 할머니가 그리는 고양이 굿즈 준비","2023-07-17")
-const notice02 = new Notice("[공지] 책방 짙은: 8월 스케줄 안내","2023-08-01")
-const notice03 = new Notice("[공지] 8/22, 23, 24 (화,수,목) 휴관합니다!","2023-08-07")
-const notice04 = new Notice("[공지] ᯽영업일 공지 안내᯽","2023-08-28")
-const notice05 = new Notice("[공지] 키즈랩 9월 프로그램 안내","2023-08-31")
-const notice06 = new Notice("[공지] 책방 짙은 9월 일정 안내","2023-09-05")
-const notice07 = new Notice("[공지] 김포아트빌리지 아트센터 추석연휴 운영안내","2023-09-26")
-const notice08 = new Notice("[전시] 우리는 공공합니다","2023-11-25")
-const notice09 = new Notice("12월 책방시간표","2023-1214")
-const notice10 = new Notice("[공지] 12월 24일 야간개장 셔틀버스 운행 안내","2023-12-21")
 
-notices=[notice01,notice02,notice03,notice04,notice05,notice06,notice07,notice08,notice08,notice10]
-noGroup=``
-
-for(const notice of notices){
-  noGroup +=`
+function makeFNoticeTemp(element){
+  return`
   <div class="swiper-slide">
-  <strong>${notice.tit}</strong>
-  <span>${notice.day}</span>
+  <strong>${element.공지명}</strong>
+  <span>${element.기간}</span>
 </div>
-  `
+  `;
 }
-fNotice.innerHTML=noGroup
--- js/sub1.js
+++ js/sub1.js
@@ -1,5 +1,4 @@
 //fetch로 json 불러오기
-
 const exBox = document.querySelector(".exhibition")
 const boBox = document.querySelector(".bookstore")
 const cuBox  = document.querySelector(".cultural-space")
@@ -9,117 +8,62 @@ fetch("../js/contents.json")
 .then((response) => response.json())
 .then((json)=> {
   exhibition = json.전시관;
+  bookStore = json.독립서점;
+  cultural = json.복합문화공간;
+  creationList = json.창작
+
    let exList = ``
    exhibition.forEach(element => {
-    exList += `
-    <li class="curation-list">
-    <a href="#">
-      <em>${element.분류}</em>
-      <address>${element.위치}</address>
-    </a>
-    <div class="curation-info">
-      <strong>${element.이름}</strong>
-      <p>${element.설명}</p>
-      <div class="curation-button">
-        <button class="mark" type="button">
-          <span class="hide">북마크</span>
-        </button>
-        <button class="like" type="button">
-          <span class="hide">마음에 들어요</span>
-        </button>              
-        <span>${element.좋아요수}</span>
-      </div>
-    </div>
-  </li>
-    `
+    exList += makeBoxTemp(element)
    });
    exBox.innerHTML=exList;
 
-   bookStore = json.독립서점;
    let boList =``
    bookStore.forEach(element => {
-    boList += `
-    <li class="curation-list">
-    <a href="#">
-      <em>${element.분류}</em>
-      <address>${element.위치}</address>
-    </a>
-    <div class="curation-info">
-      <strong>${element.이름}</strong>
-      <p>${element.설명}</p>
-      <div class="curation-button">
-        <button class="mark" type="button">
-          <span class="hide">북마크</span>
-        </button>
-        <button class="like" type="button">
-          <span class="hide">마음에 들어요</span>
-        </button>              
-        <span>${element.좋아요수}</span>
-      </div>
-    </div>
-  </li>
-    `
+    boList += makeBoxTemp(element)
    })
    boBox.innerHTML=boList
-   console.log(boBox)
 
-   cultural = json.복합문화공간;
-   cuList =``
+   let cuList =``
    cultural.forEach(element => {
-    cuList +=`
-    <li class="curation-list">
-    <a href="#">
-      <em>${element.분류}</em>
-      <address>${element.위치}</address>
-    </a>
-    <div class="curation-info">
-      <strong>${element.이름}</strong>
-      <p>${element.설명}</p>
-      <div class="curation-button">
-        <button class="mark" type="button">
-          <span class="hide">북마크</span>
-        </button>
-        <button class="like" type="button">
-          <span class="hide">마음에 들어요</span>
-        </button>              
-        <span>${element.좋아요수}</span>
-      </div>
-    </div>
-  </li>
-    `
+    cuList += makeBoxTemp(element)
    })
    cuBox.innerHTML=cuList
 
-   creationList = json.창작
-   crList =``
+   let crList =``
    creationList.forEach(element=>{
-    crList +=`
-    <li class="curation-list">
-    <a href="#">
-      <em>${element.분류}</em>
-      <address>${element.위치}</address>
-    </a>
-    <div class="curation-info">
-      <strong>${element.이름}</strong>
-      <p>${element.설명}</p>
-      <div class="curation-button">
-        <button class="mark" type="button">
-          <span class="hide">북마크</span>
-        </button>
-        <button class="like" type="button">
-          <span class="hide">마음에 들어요</span>
-        </button>              
-        <span>${element.좋아요수}</span>
-      </div>
-    </div>
-  </li>
-    `
+    crList += makeBoxTemp(element)
    })
    crBox.innerHTML=crList
 })
 
-//클릭 이벤트
+// makeBoxTemp
+function makeBoxTemp(element){
+  return `
+  <li class="curation-list">
+  <a href="#">
+    <em>${element.분류}</em>
+    <address>${element.위치}</address>
+  </a>
+  <div class="curation-info">
+    <strong>${element.이름}</strong>
+    <p>${element.설명}</p>
+    <div class="curation-button">
+      <button class="mark" type="button">
+        <span class="hide">북마크</span>
+      </button>
+      <button class="like" type="button">
+        <span class="hide">마음에 들어요</span>
+      </button>              
+      <span>${element.좋아요수}</span>
+    </div>
+  </div>
+</li>
+  `
+}
 
+
+//클릭 이벤트
 const curationBts = document.querySelectorAll(".curation > p")
 const curationBoxs = document.querySelectorAll(".curation-box > ul")
 
